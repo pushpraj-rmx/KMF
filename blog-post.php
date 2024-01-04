cc2c6cb0ec7d06715edbae5e350e26d269a5a30e
Pushpraj Dwivedi <pushprajdwivedi001@gmail.com>
2024-01-04 10:29:22 +0530

    minor-changes

--- blog-post.php
+++ blog-post.php
@@ -106,6 +106,12 @@ $query = mysqli_fetch_array(mysqli_query($conn, "select * from blog where blog_i
       </div>
       <!-- end inner -->
     </section>
+
+    <style>
+      .post-content.left {
+text-align: center;
+      }
+    </style>
     <!-- end int-hero -->
     <section class="content">
       <div class="journal">
@@ -179,42 +185,7 @@ $query = mysqli_fetch_array(mysqli_query($conn, "select * from blog where blog_i
         </div>
         <!-- end post -->
         <div class="clearfix"></div>
-        <div class="container-fluid">
-          <div class="row">
-            <div class="col-md-6 col-sm-12 wow fadeInUp">
-              <div class="post">
-                <figure><img src="images/news02.jpg" alt="Image">
-                  <div class="author"> <img src="images/team01.jpg" alt="Image"> <small>by <strong>Themezinho</strong></small> </div>
-                </figure>
-                <div class="post-content"> <small>Posted 20 July 2021 | <a href="#">Web</a>, <a href="#">UI-UX</a>, <a href="#">Coding</a></small>
-                  <h3><a href="#">Fashion fades only style remains in the same</a></h3>
-                  <p>Our developments are designed to elevate student living beyond just an accommodation space.</p>
-                  <a href="#" class="link">READ MORE</a>
-                </div>
-                <!-- end post-content -->
-              </div>
-              <!-- end post -->
-            </div>
-            <!-- end col-6 -->
-            <div class="col-md-6 col-sm-12 wow fadeInUp">
-              <div class="post">
-                <figure><img src="images/news03.jpg" alt="Image">
-                  <div class="author"> <img src="images/team03.jpg" alt="Image"> <small>by <strong>Themezinho</strong></small> </div>
-                </figure>
-                <div class="post-content"> <small>Posted 20 July 2021 | <a href="#">Web</a>, <a href="#">UI-UX</a>, <a href="#">Coding</a></small>
-                  <h3><a href="#">The new Anki Vector robot is smart </a></h3>
-                  <p>Our developments are designed to elevate student living beyond just an accommodation space.</p>
-                  <a href="#" class="link">READ MORE</a>
-                </div>
-                <!-- end post-content -->
-              </div>
-              <!-- end post -->
-            </div>
-            <!-- end col-6 -->
 
-          </div>
-          <!-- end row -->
-        </div>
         <!-- end container -->
       </div>
       <!-- end journal -->
